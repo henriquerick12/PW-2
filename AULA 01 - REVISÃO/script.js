7430afd8e692d70395ea9246584d564d559b8cd9
henrique <henriquewood3@gmail.com>
2021-07-23 13:54:43 -0300

    Lista de Exercicios Revisao

---
+++ AULA 01 - REVISÃO/script.js
@@ -0,0 +1,59 @@
+// LISTA DE EXERCICIOS
+
+// EX : 01
+let sexo = 'Masculino'
+console.log(sexo)
+
+// EX : 02
+var idade = 22
+console.log(idade)
+
+// EX : 03
+let comidaFav = null;
+console.log(comidaFav)
+
+// EX : 04
+comidaFav = 'Lasanha'
+console.log(comidaFav)
+
+// EX : 05
+let varString = 'olá'
+console.log(varString)
+
+// EX : 06
+let num = '1'
+console.log(num)
+
+// EX : 07
+let nome = 'Henrique'
+let sobreNome = 'Silva'
+let somaNS = nome + sobreNome
+console.log(somaNS)
+
+// EX : 08
+let frase = "It's Time"
+console.log(frase)
+
+// EX : 09
+let verifique = typeof(nome)
+console.log(verifique)
+
+// EX : 10
+let result = 20 + 5 + (2 / 2) + 20
+console.log(result)
+
+// EX : 11
+let n1 = 1
+let n2 = 'henrique'
+let resultN = n1 - n2
+console.log(resultN)
+
+// EX : 12
+let numeroString = '200'
+let numnumeroStringNumber = 50
+somaNumeroString = parseInt(numeroString) + numnumeroStringNumber
+console.log(somaNumeroString)
+
+// EX : 13
+let increment = 5
+console.log(`5 -> ${++increment}`)
